9726f7ce7e24e813f13299a4614778762ae3a05c
Donn Zhu <artixzhu@gmail.com>
2021-11-20 01:56:24 -0500

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer-core");
+
+const login = require('./login.js');
+const checkin = require('./checkin.js');
+
+const config = require('./config.json');
+
+
+
+async function run(config) {
+  const username = config.username;
+  const password = config.password;
+  const token = config.token;
+
+  //   const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: "/usr/bin/chromium-browser",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-web-security",
+    ],
+  });
+  const page = await browser.newPage();
+  const LOGIN_URL = "https://auth.1point3acres.com/login";
+  const CHECKIN_URL = "https://www.1point3acres.com/bbs/dsu_paulsign-sign.html";
+  await page.goto(LOGIN_URL);
+  await page.screenshot({ path: "login-loaded.png" });
+
+  if(page.url() === LOGIN_URL) await login(page, token, username, password);
+
+  await page.goto(CHECKIN_URL);
+
+  await checkin(page, token);
+
+  browser.close();
+}
+
+run(config);
---
+++ checkin.js
@@ -0,0 +1,21 @@
+const solve = require('./validate.js');
+
+async function checkin(page, token) {
+    await solve(page, token);
+    await page.screenshot({ path: "checkin-recapcha.png" });
+
+    await page.click('#kx');
+    
+    await page.focus('#todaysay')
+    await page.keyboard.type('两心大大滴淮列');
+
+    await Promise.all([
+        page.click('input[type="submit"]'),
+        page.waitForNavigation({waitUntil: 'networkidle2'})
+    ]);
+
+    await page.screenshot({ path: "checkin-checkedin.png" });
+
+}
+
+module.exports = checkin;
