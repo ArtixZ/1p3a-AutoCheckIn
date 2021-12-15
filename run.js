e9d129979c3f5c456ac47441a418d17a14bedd09
Don <artixzhu@gmail.com>
2021-12-15 14:03:11 -0500

    use process.execPath to get node executable

--- run.js
+++ run.js
@@ -1,16 +1,10 @@
 const { spawn } = require('child_process');
 const path = require('path');
-const { promisify } = require('util');
-const cmd = promisify(require('child_process').exec)
 
+function run() {
+  console.log('node path: ', process.execPath);
 
-async function run() {
-  // console.log( process.env.PATH );
-  const execPath = await cmd('which node');
-  console.log('node path: ', execPath);
-  if(execPath.stderr) throw new Error('Node JS is not installed.');
-
-  const exec = spawn(execPath.stdout.trim(), [path.join(__dirname, 'app.js')], {
+  const exec = spawn(process.execPath, [path.join(__dirname, 'app.js')], {
     env: {
       NODE_ENV: 'production',
       PATH: process.env.PATH
