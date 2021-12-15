4557cdfada71a5f6cee6c47e85c3a217535cdd8d
Don <artixzhu@gmail.com>
2021-12-14 23:58:04 -0500

    update

--- run.js
+++ run.js
@@ -1,12 +1,12 @@
 const { spawn } = require('child_process');
 const path = require('path');
 const { promisify } = require('util');
-const exec = promisify(require('child_process').exec)
+const cmd = promisify(require('child_process').exec)
 
 
 async function run() {
   // console.log( process.env.PATH );
-  const execPath = await exec('which node');
+  const execPath = await cmd('which node');
   const exec = spawn(execPath, [path.join(__dirname, 'app.js')], {
     env: {
       NODE_ENV: 'production',
