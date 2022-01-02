df5be5acb9149d7154c404316474b96319a6798e
Don <artixzhu@gmail.com>
2022-01-02 14:18:06 -0500

    update

--- run.js
+++ run.js
@@ -1,30 +1,36 @@
-const { spawn } = require('child_process');
-const path = require('path');
+const { spawn } = require("child_process");
+const path = require("path");
 
 function run() {
-  console.log('node path: ', process.execPath);
+    console.log("node path: ", process.execPath);
 
-  const exec = spawn(process.execPath, [path.join(__dirname, 'app.js')], {
-    env: {
-      NODE_ENV: 'production',
-      PATH: process.env.PATH
-    }
-  });
+    const exec = spawn(process.execPath, [path.join(__dirname, "app.js")], {
+        env: {
+            NODE_ENV: "production",
+            PATH: process.env.PATH,
+        },
+    });
 
-  exec.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
+    exec.stdout.on("data", (data) => {
+        console.log(`stdout: ${data}`);
+    });
 
-  exec.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    exec.stderr.on("data", (data) => {
+        console.error(`stderr: ${data}`);
+    });
 
-  exec.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-    if(code != 0) {
-      setTimeout(() => run(), 300000);
-    }
-  }); 
+    exec.on("close", (code) => {
+        console.log(`child process exited with code ${code}`);
+        if (code != 0) {
+            console.log("Going to restart after 5 minutes.");
+
+            setTimeout(() => {
+                console.log("Restarting...");
+
+                run();
+            }, 300000);
+        }
+    });
 }
 
 run();
